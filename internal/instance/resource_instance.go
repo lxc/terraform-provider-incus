cad1b4960fc4a78627fd76aac2f0e3be448d6995
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2024-11-19 08:24:41 +0100

    bugfix: fix instance is operational condition
    Instance is operational with a positive process count.
    
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/instance/resource_instance.go
+++ internal/instance/resource_instance.go
@@ -1301,11 +1301,11 @@ func waitForState(ctx context.Context, refreshFunc retry.StateRefreshFunc, targe
 
 // isInstanceOperational determines if an instance is fully operational based
 // on its state. It returns true if the instance is running and the reported
-// process count is positive. Checking for a positive process count is esential
+// process count is positive. Checking for a positive process count is essential
 // for virtual machines, which can report this metric only if the Incus agent has
 // started and has established a connection to the Incus server.
 func isInstanceOperational(s api.InstanceState) bool {
-	return isInstanceRunning(s) && s.Pid > 0
+	return isInstanceRunning(s) && s.Processes > 0
 }
 
 // isInstanceRunning returns true if its status is either "Running" or "Ready".
