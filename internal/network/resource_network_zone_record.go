7fad3ec6929b898f6d982ebc5115d6110f85ff74
Fabian Mettler <dev@maveonair.com>
2025-02-20 21:36:52 +0100

    network: Set default TTL for a network zone record
    As per Incus documentation, the default TTL is 300. However, when set TTL to computed and creating a new record without a specified TTL, the returned value is 0. Similar to instances, we set the Incus default in the provider to the appropriate value.
    
    Signed-off-by: Fabian Mettler <dev@maveonair.com>

--- internal/network/resource_network_zone_record.go
+++ internal/network/resource_network_zone_record.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
@@ -120,6 +121,8 @@ func (r NetworkZoneRecordResource) Schema(_ context.Context, _ resource.SchemaRe
 
 						"ttl": schema.Int64Attribute{
 							Optional:    true,
+							Computed:    true,
+							Default:     int64default.StaticInt64(300),
 							Description: "Record entry TTL",
 							Validators: []validator.Int64{
 								int64validator.AtLeast(1),
