ccd7061c1257339f799440b3ceed403ba690bc78
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-08-12 10:49:03 +0200

    tests: Add tests for multiple server resources
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/server/resource_server_test.go
+++ internal/server/resource_server_test.go
@@ -106,6 +106,59 @@ func TestAccServer_update_overwrite_pre_existing(t *testing.T) {
 	})
 }
 
+func TestAccServer_multiple_resources(t *testing.T) {
+	configUserName1 := "acctest-" + petname.Generate(2, "-")
+	configUserName2 := "acctest-" + petname.Generate(2, "-")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				PreConfig: func() { acctest.PreConfigAccTestServerConfig(t, true) }, // ensures, that "acctest-pre-existing.key" already exists in the config and that it remains after destroy.
+				Config:    testAccMultipleResources1(configUserName1, configUserName2),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.create", configUserName1), "value"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.update", configUserName1), "value"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.remove", configUserName1), "value"),
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.create", configUserName2)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.update", configUserName2)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.remove", configUserName2)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), "config.user.acctest-pre-existing.key"),                // pre existing, not managed through Terraform.
+
+					resource.TestCheckResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.create", configUserName2), "value"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.update", configUserName2), "value"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.remove", configUserName2), "value"),
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.create", configUserName1)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.update", configUserName1)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.remove", configUserName1)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), "config.user.acctest-pre-existing.key"),                // pre existing, not managed through Terraform.
+				),
+			},
+			{
+				Config: testAccMultipleResources2(configUserName1, configUserName2),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.create", configUserName1), "value"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.update", configUserName1), "new_value"),
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.remove", configUserName1)),
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.create", configUserName2)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.update", configUserName2)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), fmt.Sprintf("config.user.%s.remove", configUserName2)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName1), "config.user.acctest-pre-existing.key"),                // pre existing, not managed through Terraform.
+
+					resource.TestCheckResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.create", configUserName2), "value"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.update", configUserName2), "new_value"),
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.remove", configUserName2)),
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.create", configUserName1)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.update", configUserName1)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), fmt.Sprintf("config.user.%s.remove", configUserName1)), // Not managed by this resource.
+					resource.TestCheckNoResourceAttr(fmt.Sprintf("incus_server.%s", configUserName2), "config.user.acctest-pre-existing.key"),                // pre existing, not managed through Terraform.
+				),
+			},
+		},
+	})
+}
+
 func testAccServer1(configLoggingName string) string {
 	return fmt.Sprintf(`
 resource "incus_server" "%[1]s" {
@@ -141,4 +194,44 @@ resource "incus_server" "%[1]s" {
   }
 }
 `, configLoggingName)
+}
+
+func testAccMultipleResources1(configLoggingName1 string, configLoggingName2 string) string {
+	return fmt.Sprintf(`
+resource "incus_server" "%[1]s" {
+  config = {
+    "user.%[1]s.create" = "value"
+    "user.%[1]s.update" = "value"
+    "user.%[1]s.remove" = "value"
+  }
+}
+
+resource "incus_server" "%[2]s" {
+  config = {
+    "user.%[2]s.create" = "value"
+    "user.%[2]s.update" = "value"
+    "user.%[2]s.remove" = "value"
+  }
+}
+`, configLoggingName1, configLoggingName2)
+}
+
+func testAccMultipleResources2(configLoggingName1 string, configLoggingName2 string) string {
+	return fmt.Sprintf(`
+resource "incus_server" "%[1]s" {
+  config = {
+    "user.%[1]s.create"    = "value"
+    "user.%[1]s.update"    = "new_value" // updated
+    // "user.%[1]s.remove" = "value" // removed (commented out)
+  }
+}
+
+resource "incus_server" "%[2]s" {
+  config = {
+    "user.%[2]s.create"    = "value"
+    "user.%[2]s.update"    = "new_value" // updated
+    // "user.%[2]s.remove" = "value" // removed (commented out)
+  }
+}
+`, configLoggingName1, configLoggingName2)
 }
