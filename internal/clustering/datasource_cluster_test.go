78c146b102ad9caf3cf37ca68440567c43147595
Lucas Bremgartner <lucas.bremgartner@futurfusion.io>
2025-08-14 06:15:46 +0200

    tests: Remove fallback for non-clustered case
    Signed-off-by: Lucas Bremgartner <lucas.bremgartner@futurfusion.io>

--- internal/clustering/datasource_cluster_test.go
+++ internal/clustering/datasource_cluster_test.go
@@ -21,17 +21,7 @@ func TestAccClusterDataSource_Standalone(t *testing.T) {
 				Config: `data "incus_cluster" "test" {}`,
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("data.incus_cluster.test", "is_clustered", "false"),
-					resource.TestCheckResourceAttr("data.incus_cluster.test", "members.%", "1"),
-					// Since the name of the cluster member is the empty string in the
-					// non-clustered case, there are two consecutive dots to address the properties.
-					resource.TestCheckResourceAttr("data.incus_cluster.test", "members..%", "7"),                                                       // Expected attributes per cluster member.
-					resource.TestMatchResourceAttr("data.incus_cluster.test", "members..address", regexp.MustCompile(`(^https://.+:[1-9][0-9]*$|^$)`)), // Expect valid URL or empty string (if Incus is not bound to network and only controlled via unix socket).
-					// Architecture list from https://linuxcontainers.org/incus/docs/main/architectures/
-					resource.TestMatchResourceAttr("data.incus_cluster.test", "members..architecture", regexp.MustCompile(`i686|x86_64|armv6l|armv7l|armv8l|aarch64|ppc|ppc64|ppc64le|s390x|mips|mips64|riscv32|riscv64|loongarch64`)), // Expect architecture to have a supported value
-					resource.TestCheckResourceAttr("data.incus_cluster.test", "members..failure_domain", ""),                                                                                                                           // Failure domain is undefined for standalone instances.
-					resource.TestCheckResourceAttr("data.incus_cluster.test", "members..groups.#", "0"),                                                                                                                                // Cluster member groups are undefined for standalone instances.
-					resource.TestCheckResourceAttr("data.incus_cluster.test", "members..roles.#", "0"),                                                                                                                                 // Cluster member roles are undefined for standalone instances.
-					resource.TestCheckResourceAttr("data.incus_cluster.test", "members..status", "Online"),                                                                                                                             // Standalone instances are always considered online, if fetch data is successful.
+					resource.TestCheckResourceAttr("data.incus_cluster.test", "members.%", "0"),
 				),
 			},
 		},
