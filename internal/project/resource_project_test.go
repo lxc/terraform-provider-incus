6f3ef4bb847d6a6ad2266019b7911d4f0c9cc31c
David Schneider <david@schndr.dev>
2025-08-20 22:29:20 +0200

    tests: Add tests for force destroying project
    Signed-off-by: David Schneider <david@schndr.dev>

--- internal/project/resource_project_test.go
+++ internal/project/resource_project_test.go
@@ -28,6 +28,7 @@ func TestAccProject_basic(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("incus_project.project0", "name", projectName),
 					resource.TestCheckResourceAttr("incus_project.project0", "description", "Terraform provider test project"),
+					resource.TestCheckResourceAttr("incus_project.project0", "force_destroy", "false"),
 					// Ensure state of computed keys is not tracked.
 					resource.TestCheckNoResourceAttr("incus_project.project0", "config.features.images"),
 					resource.TestCheckNoResourceAttr("incus_project.project0", "config.features.profiles"),
@@ -52,6 +53,7 @@ func TestAccProject_config(t *testing.T) {
 					resource.TestCheckResourceAttr("incus_project.project1", "name", projectName),
 					resource.TestCheckResourceAttr("incus_project.project1", "config.features.images", "true"),
 					resource.TestCheckResourceAttr("incus_project.project1", "config.features.profiles", "false"),
+					resource.TestCheckResourceAttr("incus_project.project1", "force_destroy", "false"),
 					// Ensure state of computed keys is not tracked.
 					resource.TestCheckNoResourceAttr("incus_project.project1", "config.features.storage.volumes"),
 					resource.TestCheckNoResourceAttr("incus_project.project1", "config.features.storage.buckets"),
@@ -75,6 +77,7 @@ func TestAccProject_updateConfig(t *testing.T) {
 					resource.TestCheckResourceAttr("incus_project.project1", "description", "Old description"),
 					resource.TestCheckResourceAttr("incus_project.project1", "config.features.images", "true"),
 					resource.TestCheckResourceAttr("incus_project.project1", "config.features.profiles", "false"),
+					resource.TestCheckResourceAttr("incus_project.project1", "force_destroy", "false"),
 					// Ensure state of computed keys is not tracked.
 					resource.TestCheckNoResourceAttr("incus_project.project1", "config.features.storage.volumes"),
 					resource.TestCheckNoResourceAttr("incus_project.project1", "config.features.storage.buckets"),
@@ -87,9 +90,54 @@ func TestAccProject_updateConfig(t *testing.T) {
 					resource.TestCheckResourceAttr("incus_project.project1", "description", "New description"),
 					resource.TestCheckResourceAttr("incus_project.project1", "config.features.images", "false"),
 					resource.TestCheckResourceAttr("incus_project.project1", "config.features.profiles", "true"),
+					resource.TestCheckResourceAttr("incus_project.project1", "force_destroy", "false"),
 					// Ensure state of computed keys is not tracked.
 					resource.TestCheckNoResourceAttr("incus_project.project1", "config.features.storage.volumes"),
 					resource.TestCheckNoResourceAttr("incus_project.project1", "config.features.storage.buckets"),
+				),
+			},
+			{
+				Config: testAccProject_updateConfig_3(projectName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("incus_project.project1", "name", projectName),
+					resource.TestCheckResourceAttr("incus_project.project1", "description", "New description"),
+					resource.TestCheckResourceAttr("incus_project.project1", "force_destroy", "true"),
+					resource.TestCheckResourceAttr("incus_project.project1", "config.features.images", "false"),
+					resource.TestCheckResourceAttr("incus_project.project1", "config.features.profiles", "true"),
+					// Ensure state of computed keys is not tracked.
+					resource.TestCheckNoResourceAttr("incus_project.project1", "config.features.storage.volumes"),
+					resource.TestCheckNoResourceAttr("incus_project.project1", "config.features.storage.buckets"),
+				),
+			},
+		},
+	})
+}
+
+
+// force_destroy is tested by creating a project containing an untracked instance
+// and letting CheckDestroy verify that the project can be destroyed.
+func TestAccProject_forceDestroy(t *testing.T) {
+	projectName := petname.Generate(2, "-")
+	instanceName := petname.Generate(2, "-")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(t) },
+		ProtoV6ProviderFactories: acctest.ProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccProject_forceDestroy_config(projectName, instanceName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("incus_project.project1", "name", projectName),
+					resource.TestCheckResourceAttr("incus_project.project1", "force_destroy", "true"),
+					resource.TestCheckResourceAttr("incus_instance.instance1", "name", instanceName),
+					resource.TestCheckResourceAttr("incus_instance.instance1", "project", projectName),
+				),
+			},
+			{
+				Config: testAccProject_forceDestroy_danglingInstance(projectName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("incus_project.project1", "name", projectName),
+					resource.TestCheckResourceAttr("incus_project.project1", "force_destroy", "true"),
 				),
 			},
 		},
@@ -181,3 +229,60 @@ resource "incus_project" "project1" {
   }
 }`, name)
 }
+
+func testAccProject_updateConfig_3(name string) string {
+	return fmt.Sprintf(`
+resource "incus_project" "project1" {
+  name        = "%s"
+  description = "New description"
+  force_destroy = true
+  config = {
+	"features.images"   = false
+	"features.profiles" = true
+  }
+}`, name)
+}
+
+func testAccProject_forceDestroy_config(name string, instanceName string) string {
+	return fmt.Sprintf(`
+resource "incus_project" "project1" {
+  name          = "%s"
+  description   = "Terraform provider test project"
+  force_destroy = true
+  config = {
+	"features.images"   = true
+	"features.profiles" = false
+  }
+}
+
+resource "incus_instance" "instance1" {
+  name   = "%s"
+  project = incus_project.project1.name
+  type = "container"
+  image  = "%s"
+}
+`, name, instanceName, acctest.TestImage)
+}
+
+func testAccProject_forceDestroy_danglingInstance(name string) string {
+	return fmt.Sprintf(`
+resource "incus_project" "project1" {
+  name          = "%s"
+  description   = "Terraform provider test project"
+  force_destroy = true
+  config = {
+	"features.images"   = true
+	"features.profiles" = false
+  }
+}
+
+removed {
+  from = incus_instance.instance1
+  lifecycle {
+	destroy = false
+  }
+}
+`, name)
+}
+
+
